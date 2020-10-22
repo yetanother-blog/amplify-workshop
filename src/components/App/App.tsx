d58ad6d7d20bc8d82f0e9880d88722e8dc3677e2
Henrik Fricke <henrikfricke@web.de>
2020-10-22 17:45:24 +0200

    feat: add material ui (#4)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Header } from '../Header/Header';
+
+export const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+    </>
+  );
+}
