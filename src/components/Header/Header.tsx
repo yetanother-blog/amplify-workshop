eff84c0e0d987386fcebd03538340f14a2a4bd46
Henrik Fricke <henrikfricke@web.de>
2020-10-25 23:22:21 +0100

    refactor(header): remove menu icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { AppBar, IconButton, Button, Toolbar, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import { Auth } from 'aws-amplify';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -29,9 +28,6 @@ export const Header = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Cookbook
           </Typography>
