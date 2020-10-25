8dfbf10fad1b040809478c2f1fed08fe093797a9
Henrik Fricke <henrikfricke@web.de>
2020-10-25 19:43:55 +0100

    feat: auth ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
 import { Header } from '../Header/Header';
+import './App.css';
 
 export const App = () => {
   return (
-    <>
+    <AmplifyAuthenticator>
       <CssBaseline />
       <Header />
-    </>
+    </AmplifyAuthenticator>
   );
 };
